2253b201e9d932d6bcabf8cd797e897ef3739188
faosantos <gutsdevloper@gmail.com>
2021-05-24 17:10:34 -0300

    comitt aula 40

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -27,15 +27,27 @@ Route::get('/contato', function () {
     return 'Contato';
 });*/
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
-Route::get('/login', function (){ return 'Login'; });
+Route::get('/login', function (){ return 'Login'; })->name('site.login');
 
 
 Route::prefix('/app')->group(function() {
-Route::get('/clientes', function (){ return 'Clientes'; });
-Route::get('/fornecedores', function (){ return 'Fornecedores'; });
-Route::get('/produtos', function (){ return 'Produtos'; });
+Route::get('/clientes', function (){ return 'Clientes'; })->name('app.clientes');
+Route::get('/fornecedores', function (){ return 'Fornecedores'; })->name('app.fornecedores');
+Route::get('/produtos', function (){ return 'Produtos'; })->name('app.produtos');
+});
+
+
+//rota de teste
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+
+//rota de fallback - não retorna um erro, mas sim alguma informação costumizada para o usurio final.
+Route::fallback(function(){
+
+    echo'A rota acessada não existe. <a href="' .route('site.index').'">Clique aqui</a> para ir para a página inicial.';
+
 });
