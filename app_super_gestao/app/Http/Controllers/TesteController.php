ec58a114ec7e58197dc55fd600baecc88ad4a268
faosantos <gutsdevloper@gmail.com>
2021-05-25 16:40:44 -0300

    commitAula 42

--- app_super_gestao/app/Http/Controllers/TesteController.php
+++ app_super_gestao/app/Http/Controllers/TesteController.php
@@ -8,7 +8,10 @@ class TesteController extends Controller
 {
     public function teste(int $p1, int $p2){
 
-        echo "A soma de $p1 + $p2 é: ".($p1+$p2);
+       // echo "A soma de $p1 + $p2 é: ".($p1+$p2);
+       //return view('site.teste', ['p1'=>$p1], ['p2'=>$p2] ); //array associativo
+
+        return view('site.teste', compact('p1', 'p2')); //compact
 
     }
 }
