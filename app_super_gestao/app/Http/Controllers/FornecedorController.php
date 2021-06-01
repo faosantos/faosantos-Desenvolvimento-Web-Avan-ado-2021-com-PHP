9fb143fc4839868209b16e30a3d5795d5f88ba30
faosantos <gutsdevloper@gmail.com>
2021-06-01 16:45:38 -0300

    commit aula 47

---
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FornecedorController extends Controller
+{
+    public function index() {
+
+        return view('app.fornecedor.index');
+
+    }
+}
