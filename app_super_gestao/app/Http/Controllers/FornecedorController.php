857ab5b8b7991124bbc5e0bcfae091be31a85781
faosantos <gutsdevloper@gmail.com>
2021-12-23 14:49:53 -0300

    faosantos

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,29 @@ class FornecedorController extends Controller
 {
     public function index() {
 
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            0 => ['nome' => 'Fornecedor 1', 
+                'status' =>'N',
+                'cnpj' => '0',
+                'ddd' => '', //São Paulo(SP)
+                'telefone' => '0000-0000'
+            ],
+            1 => ['nome' => 'Fornecedor 2', 
+                'status' =>'S',
+                'cnpj' => null,
+                'ddd' => '85', //Fortaleza(CE)
+                'telefone' => '0000-0000'
+            ],
+            2 => ['nome' => 'Fornecedor 3', 
+            'status' =>'S',
+            'cnpj' => null,
+            'ddd' => '21', //Rio de Janeiro(RJ)
+            'telefone' => '0000-0000'
+            ]
+        
+        ];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
 
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,1 +1,32 @@
+
 <h3>Fornecedor</h3>
+
+ @php
+    /*
+    if(empty($variavel)) {} //retornar true se a variável estiver vazia
+    - ''
+    - 0
+    - 0.0
+    - '0'
+    - null
+    - false
+    - array()
+    - $var
+    */
+@endphp
+
+@isset($fornecedores)
+    
+    @foreach ($fornecedores as $indice => $fornecedor)
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <hr>
+      
+    @endforeach
+    
+@endisset
